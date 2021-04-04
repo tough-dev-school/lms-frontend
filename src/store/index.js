b8592e3412c0f3758b3b0ab0fee9672b97443d9b
Fedor Borshev <fedor@borshev.com>
2021-04-04 17:13:31 +0300

    Storing auth data in cookies

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import * as Cookies from "js-cookie";
 
 Vue.use(Vuex);
 
@@ -9,4 +11,16 @@ export default new Vuex.Store({
   modules: {
     auth,
   },
+  plugins: [
+    createPersistedState({
+      paths: ["auth"],
+      storage: {
+        getItem: (key) => {
+          return Cookies.get(key);
+        },
+        setItem: (key, value) => Cookies.set(key, value, { expires: 365, secure: false }),
+        removeItem: (key) => Cookies.remove(key),
+      },
+    }),
+  ],
 });
