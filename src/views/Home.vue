887f97e8e5725058076f5a1378b1168f1ac8573f
Fedor Borshev <fedor@borshev.com>
2021-04-11 16:44:53 +0300

    User data field refactoring

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,72 +1,29 @@
 <template>
   <div>
     <form v-if="isUserFetched" @submit.prevent="submit">
-      <label for="firstName"
-        >Имя
-        <input id="firstName" v-model="first_name" type="text" />
-      </label>
-      <label for="lastName"
-        >Фамилия
-        <input id="lastName" v-model="last_name" type="text" />
-      </label>
-      <label for="firstNameEn"
-        >Имя (на английском)
-        <input id="firstNameEn" v-model="first_name_en" type="text" />
-      </label>
-      <label for="lastNameEn"
-        >Фамилия(на английском)
-        <input id="lastNameEn" v-model="last_name_en" type="text" />
-      </label>
+      <UserDataField label="Имя" name="first_name" />
+      <UserDataField label="Фамилия" name="last_name" />
+      <UserDataField label="Имя (на английском)" name="first_name_en" />
+      <UserDataField label="Фамилия(на английском)" name="last_name_en" />
+
       <input type="submit" value="Обновить" :disabled="isUpdating" />
     </form>
   </div>
 </template>
 
 <script>
-import { mapState, mapActions, mapMutations } from "vuex";
+import { mapActions } from "vuex";
+
+import UserDataField from "@/components/Home/UserDataField";
 
 export default {
   name: "Home",
+  components: { UserDataField },
   data() {
     return {
       isUserFetched: false,
       isUpdating: false,
     };
-  },
-  computed: {
-    ...mapState("user", ["user"]),
-    first_name: {
-      get() {
-        return this.user.first_name;
-      },
-      set(first_name) {
-        this.UPDATE_USER({ first_name });
-      },
-    },
-    last_name: {
-      get() {
-        return this.user.last_name;
-      },
-      set(last_name) {
-        this.UPDATE_USER({ last_name });
-      },
-    },
-    first_name_en: {
-      get() {
-        return this.user.first_name_en;
-      },
-      set(first_name_en) {
-        this.UPDATE_USER({ first_name_en });
-      },
-    },
-    last_name_en: {
-      get() {
-        return this.user.last_name_en;
-      },
-      set(last_name_en) {
-        this.UPDATE_USER({ last_name_en });
-      },
-    },
   },
   async created() {
     await this.FETCH_USER();
@@ -74,7 +31,6 @@ export default {
   },
   methods: {
     ...mapActions("user", ["FETCH_USER", "UPDATE_BACKEND"]),
-    ...mapMutations("user", ["UPDATE_USER"]),
     async submit() {
       this.isUpdating = true;
       await this.UPDATE_BACKEND();
