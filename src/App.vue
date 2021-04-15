666c1548cc5b42463e3e03ddcf39bdbf615930fd
Fedor Borshev <fedor@borshev.com>
2021-04-15 19:18:29 +0300

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,26 @@
 <template>
-  <div id="app" class="container">
-    <router-view />
+  <div id="app">
+    <TheNavBar v-if="isAuthenticated" />
+    <div class="container">
+      <router-view />
+    </div>
   </div>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+
+import TheNavBar from "@/components/TheNavBar.vue";
+
+export default {
+  components: {
+    TheNavBar,
+  },
+  computed: mapGetters("auth", ["isAuthenticated"]),
+};
+</script>
+<style scoped>
+.container {
+  padding-top: 10rem;
+}
+</style>
