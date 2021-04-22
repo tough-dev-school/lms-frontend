e90159347f6eb84d789b288eb2a4cb6ae940e189
Fedor Borshev <fedor@borshev.com>
2021-04-23 01:11:07 +0300

    Added sentry

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from "vue";
+import * as Sentry from "@sentry/vue";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    Vue,
+    dsn: "https://ba238a6269f742aab98f715c334fbc36@o47144.ingest.sentry.io/5731954",
+  });
+}
 Vue.config.productionTip = false;
 
 new Vue({
