528622b1602438210adb3a0b4c05750949bd96f1
Fedor Borshev <fedor@borshev.com>
2021-05-02 16:59:52 +0300

    Disabled typography in Safari

--- src/utils/richtypo.js
+++ src/utils/richtypo.js
@@ -1,4 +1,11 @@
-import richtypo from "richtypo";
-import rules from "richtypo-rules-ru";
+export default (text) => {
+  try {
+    const richtypo = require("richtypo");
+    const rules = require("richtypo-rules-ru");
 
-export default (text) => richtypo(rules, text);
+    return richtypo(rules, text);
+  } catch {
+    // workaround for https://github.com/sapegin/richtypo.js/issues/34, when richtypo fails on safari
+    return text;
+  }
+};
