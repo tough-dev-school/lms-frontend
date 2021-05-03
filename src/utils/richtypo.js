ffee23356f9e2dcf1f0fc6052d11640c0fdfc157
Fedor Borshev <fedor@borshev.com>
2021-05-03 17:37:01 +0300

    closes f213/education-backend#297 -- migrated to the lower richtypo version which is able to run in browser

--- src/utils/richtypo.js
+++ src/utils/richtypo.js
@@ -1,11 +1,4 @@
+import richtypo from "richtypo";
 export default (text) => {
-  try {
-    const richtypo = require("richtypo");
-    const rules = require("richtypo-rules-ru");
-
-    return richtypo(rules, text);
-  } catch {
-    // workaround for https://github.com/sapegin/richtypo.js/issues/34, when richtypo fails on safari
-    return text;
-  }
+  return richtypo.rich(text, "ru");
 };
