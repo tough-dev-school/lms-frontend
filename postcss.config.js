9a692f30ee5c04b111a9b4f9e9c759e503be699e
jinonensk <jinonensk@gmail.com>
2021-09-23 04:15:13 +0700

    2 - new deps and new config postcss rules

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,10 @@
+const path = require("path");
+
 module.exports = {
   plugins: [
+    require("postcss-mixins")({
+      mixinsFiles: path.join(__dirname, "src/assets/styles/mixins"),
+    }),
     require("postcss-nested"),
     require("postcss-custom-media")({
       importFrom: [
