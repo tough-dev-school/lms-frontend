e21cf75d7992036f9108f2975f4c03dd4b68fd5d
Fedor Borshev <fedor@borshev.com>
2021-12-15 21:33:35 +0300

    Address change

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/lms/" : "/",
-  outputDir: "dist/lms",
   devServer: {
     port: 3000,
     proxy: "http://localhost:8000",
