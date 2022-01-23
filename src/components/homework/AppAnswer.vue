5db100c283b1e67fb4d60bc53927def1e7ea45e7
Fedor Borshev <fedor@borshev.com>
2022-01-23 19:16:27 +0300

    Highlighting answer as deleted during deletion

--- src/components/homework/AppAnswer.vue
+++ src/components/homework/AppAnswer.vue
@@ -1,9 +1,9 @@
 <template>
-  <div :id="label" class="answer">
+  <div :id="label" class="answer" :class="{ 'answer--deleted': isDeleted }">
     <div class="answer__author-wapper">
       <AppUserAvatar v-if="!isChild" :user="answer.author" :color="currentColor" class="answer-editor__avatar" />
       <AppUserName :user="answer.author" :color="currentColor" class="answer__user-name" font="inter" />
-      <AppAnswerDeleteButton :answer="answer" v-on="$listeners" />
+      <AppAnswerDeleteButton :answer="answer" @deleted="deleted" />
     </div>
     <div class="answer__text">
       <AppContent :html="answer.text" />
@@ -30,6 +30,11 @@ export default {
     answer: { type: Object, required: true },
     isChild: { type: Boolean, default: false },
   },
+  data() {
+    return {
+      isDeleted: false,
+    };
+  },
   computed: {
     ...mapState("auth", ["user"]),
     label() {
@@ -42,16 +47,28 @@ export default {
       return this.isUserComment ? "secondary" : "primary";
     },
   },
+  methods: {
+    deleted(answer) {
+      this.isDeleted = true;
+      this.$emit("deleted", answer);
+    },
+  },
 };
 </script>
 <style lang="postcss" scoped>
-.answer__author-wapper {
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  margin-bottom: 8px;
-}
-.answer__user-name {
-  margin-right: 0.6rem;
+.answer {
+  &--deleted {
+    opacity: 0.5;
+    pointer-events: none;
+  }
+  &__author-wapper {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    margin-bottom: 8px;
+  }
+  &__user-name {
+    margin-right: 0.6rem;
+  }
 }
 </style>
